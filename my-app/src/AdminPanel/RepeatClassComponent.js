1941878ccc2105ebe947bdb014a1d1224b853e1d
harshpratapmall <66236930+harshpratapmall@users.noreply.github.com>
2021-02-05 18:13:15 +0530

    05/02/2021

---
+++ my-app/src/AdminPanel/RepeatClassComponent.js
@@ -0,0 +1,166 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Schedule from '@material-ui/icons/Schedule';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8,4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+ 
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="md">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <Schedule />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Set up to repeat class
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+         
+            <Grid item xs={12} >
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="Subject"
+                label="Select a class"
+                name="subject"
+                autoComplete=""
+              />
+            </Grid>
+            <Grid item xs={4} >
+              <TextField
+                variant="filled"
+                required
+                fullWidth
+                label="Date:-"
+                disabled
+              />
+            </Grid>
+            <Grid item xs={8} >
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                type="date"
+              />
+              </Grid>
+            <Grid item xs={4} >
+              <TextField
+                variant="filled"
+                required
+                fullWidth
+                label="Starting Time:-"
+                disabled
+              />
+            </Grid>
+            
+            <Grid item xs={8} >
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="S_time"
+                type="time"
+                name="S_time"
+              />
+            </Grid>
+            
+            <Grid item xs={4} >
+              <TextField
+                variant="filled"
+                required
+                fullWidth
+                label="Closing Time:-"
+                disabled
+              />
+            </Grid>
+            <Grid item xs={8} >
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="C_time"
+                type="time"
+                name="C_time"
+              />
+            </Grid>
+            <Grid item xs={12} >
+              <TextField
+                variant="filled"
+                required
+                fullWidth
+                label="List of Students:-"
+                disabled
+              />
+            </Grid>
+            <ul>
+            <li>
+              a
+            </li>
+            <li>
+              b
+            </li>
+            <li>
+              c
+            </li>
+            <li>
+              d
+            </li></ul>
+
+            <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            ADD NEW STUDENTS
+          </Button>
+            
+            <Grid item xs={12}>
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            DONE
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
+}
